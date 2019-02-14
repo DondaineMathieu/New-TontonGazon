e877cd3ae7b06eae737abf8220cd1109bde2e686
DondaineMathieu <dondaine.mathieu@gmail.com>
2019-02-14 12:46:16 +0100

    ajout gulp

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+    return gulp.src('./ressources/sass/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./site/css'));
+  });
+
+  gulp.task('autoprefixer', function () {
+    var postcss      = require('gulp-postcss');
+    var sourcemaps   = require('gulp-sourcemaps');
+    var autoprefixer = require('autoprefixer');
+  
+    return gulp.src('./site/css/style.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([ autoprefixer() ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./site/css'));
+  });
+
+  gulp.task('watch', function(){
+    gulp.watch('./sass/**/*.scss', gulp.series('sass','autoprefixer'));
+  })
