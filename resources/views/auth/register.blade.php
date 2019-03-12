5bcdc688ae06b8820a760dafb6e5cd08291bbac6
DondaineMathieu <dondaine.mathieu@gmail.com>
2019-03-12 18:22:41 +0100

    css inscription

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="card-header">{{ __('Register') }}</div>
-    <div class="card-body">
+<div class="form-container">
+    <h1>{{ __('Register') }}</h1>
+
         <form method="POST" action="{{ route('register') }}">
             @csrf
             
-            {{-- Nom de l'utilisateur --}}
-            <div class="form-group row">
-                <label for="nom" class="col-md-4 col-form-label text-md-right">{{ __('Nom') }}</label>
+            <div class="form-group">
+                <label for="nom" class="form-label">{{ __('Nom') }}</label>
 
-                <div class="col-md-6">
-                    <input id="nom" type="text" class="form-control{{ $errors->has('nom') ? ' is-invalid' : '' }}" name="nom" value="{{ old('nom') }}" required autofocus>
+                <div class="form-group-input">
+                    <input id="nom" type="text" class="form-control{{ $errors->has('nom') ? ' is-invalid' : '' }}" 
+                        name="nom" value="{{ old('nom') }}" required autofocus>
 
                     @if ($errors->has('nom'))
                     <span class="invalid-feedback" role="alert">
@@ -21,14 +21,13 @@
                     @endif
                 </div>
             </div>
-            {{-- Fin nom de l'utilisateur --}}
 
-            {{-- prenom de l'utilisateur --}}
-            <div class="form-group row">
-                <label for="prenom" class="col-md-4 col-form-label text-md-right">{{ __('Prenom') }}</label>
+            <div class="form-group">
+                <label for="prenom" class="form-label">{{ __('Prenom') }}</label>
 
-                <div class="col-md-6">
-                    <input id="prenom" type="text" class="form-control{{ $errors->has('prenom') ? ' is-invalid' : '' }}" name="prenom" value="{{ old('prenom') }}" required>
+                <div class="form-group-input">
+                    <input id="prenom" type="text" class="form-control{{ $errors->has('prenom') ? ' is-invalid' : '' }}" 
+                        name="prenom" value="{{ old('prenom') }}" required>
 
                     @if ($errors->has('prenom'))
                     <span class="invalid-feedback" role="alert">
@@ -37,13 +36,11 @@
                     @endif
                 </div>
             </div>
-            {{-- Fin prenom de l'utilisateur --}}
 
-            {{-- mail de l'utilisateur --}}
-            <div class="form-group row">
-                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Adresse mail') }}</label>
+            <div class="form-group">
+                <label for="email" class="form-label">{{ __('Adresse mail') }}</label>
 
-                <div class="col-md-6">
+                <div class="form-group-input">
                     <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
 
                     @if ($errors->has('email'))
@@ -53,13 +50,11 @@
                     @endif
                 </div>
             </div>
-            {{-- Fin mail de l'utilisateur --}}
 
-            {{-- tel de l'utilisateur --}}
-            <div class="form-group row">
-                <label for="tel" class="col-md-4 col-form-label text-md-right">{{ __('Telephone') }}</label>
+            <div class="form-group">
+                <label for="tel" class="form-label">{{ __('Telephone') }}</label>
 
-                <div class="col-md-6">
+                <div class="form-group-input">
                     <input id="tel" type="tel" class="form-control{{ $errors->has('tel') ? ' is-invalid' : '' }}" name="tel" value="{{ old('tel') }}" required>
 
                     @if ($errors->has('tel'))
@@ -72,10 +67,10 @@
             {{-- Fin tel de l'utilisateur --}}
 
             {{-- Mot de passe de l'utilisateur --}}
-            <div class="form-group row">
-                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Mot de passe') }}</label>
+            <div class="form-group">
+                <label for="password" class="form-label">{{ __('Mot de passe') }}</label>
 
-                <div class="col-md-6">
+                <div class="form-group-input">
                     <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
                     @if ($errors->has('password'))
@@ -88,21 +83,21 @@
             {{-- Fin Mot de passe de l'utilisateur --}}
 
             {{-- Confirm mot de passe de l'utilisateur --}}
-            <div class="form-group row">
+            <div class="form-group">
                 <label for="password-confirm"
-                    class="col-md-4 col-form-label text-md-right">{{ __('Confirmer le mot de passe') }}</label>
+                    class="form-label">{{ __('Confirmer le mot de passe') }}</label>
 
-                <div class="col-md-6">
+                <div class="form-group-input">
                     <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                 </div>
             </div>
             {{-- Fin Confirm mot de passe de l'utilisateur --}}
 
             {{-- adresse de l'utilisateur --}}
-            <div class="form-group row">
-                <label for="adresse1" class="col-md-4 col-form-label text-md-right">{{ __('Adresse') }}</label>
+            <div class="form-group">
+                <label for="adresse1" class="form-label">{{ __('Adresse') }}</label>
 
-                <div class="col-md-6">
+                <div class="form-group-input">
                     <input id="adresse1" type="text" class="form-control{{ $errors->has('adresse1') ? ' is-invalid' : '' }}" name="adresse1" value="{{ old('adresse1') }}" required>
 
                     @if ($errors->has('adresse1'))
@@ -115,10 +110,10 @@
             {{-- Fin adresse de l'utilisateur --}}
 
             {{-- complement adresse de l'utilisateur --}}
-            <div class="form-group row">
-                <label for="adresse2" class="col-md-4 col-form-label text-md-right">{{ __('Complément d\'Adresse') }}</label>
+            <div class="form-group">
+                <label for="adresse2" class="form-label">{{ __('Complément d\'Adresse') }}</label>
 
-                <div class="col-md-6">
+                <div class="form-group-input">
                     <input id="adresse2" type="text" class="form-control{{ $errors->has('adresse2') ? ' is-invalid' : '' }}" name="adresse2" value="{{ old('adresse2') }}">
 
                     @if ($errors->has('adresse2'))
@@ -131,10 +126,10 @@
             {{-- Fin complement adresse de l'utilisateur --}}
 
             {{-- ville de l'utilisateur --}}
-            <div class="form-group row">
-                <label for="ville" class="col-md-4 col-form-label text-md-right">{{ __('Ville') }}</label>
+            <div class="form-group">
+                <label for="ville" class="form-label">{{ __('Ville') }}</label>
 
-                <div class="col-md-6">
+                <div class="form-group-input">
                     <input id="ville" type="text" class="form-control{{ $errors->has('ville') ? ' is-invalid' : '' }}" name="ville" value="{{ old('ville') }}" required>
 
                     @if ($errors->has('ville'))
@@ -147,10 +142,10 @@
             {{-- Fin ville de l'utilisateur --}}
 
             {{-- code_postal de l'utilisateur --}}
-            <div class="form-group row">
-                <label for="code_postal" class="col-md-4 col-form-label text-md-right">{{ __('Code Postal') }}</label>
+            <div class="form-group">
+                <label for="code_postal" class="form-label">{{ __('Code Postal') }}</label>
 
-                <div class="col-md-6">
+                <div class="form-group-input">
                     <input id="code_postal" type="text" class="form-control{{ $errors->has('code_postal') ? ' is-invalid' : '' }}" name="code_postal" value="{{ old('code_postal') }}" required>
 
                     @if ($errors->has('code_postal'))
@@ -163,18 +158,18 @@
             {{-- Fin code_postal de l'utilisateur --}}
             
             {{-- role de l'utilisateur --}}
-            <div class="form-group row">
+            <div class="form-group">
 
-                <div class="col-md-6">
-                    <input id="role_tondu" type="radio" class="form-control{{ $errors->has('code_postal') ? ' is-invalid' : '' }}" name="role" value="tondu"> <label for="role_tondu" class="col-md-4 col-form-label text-md-right">{{ __('Tondu') }}</label>
-                    <input id="role_tondeur" type="radio" class="form-control{{ $errors->has('code_postal') ? ' is-invalid' : '' }}" name="role" value="tondeur"> <label for="role_tondeur" class="col-md-4 col-form-label text-md-right">{{ __('Tondeur') }}</label>
+                <div class="form-group-input">
+                    <input id="role_tondu" type="radio" class="form-control{{ $errors->has('code_postal') ? ' is-invalid' : '' }}" name="role" value="tondu"> <label for="role_tondu" class="form-label">{{ __('Tondu') }}</label>
+                    <input id="role_tondeur" type="radio" class="form-control{{ $errors->has('code_postal') ? ' is-invalid' : '' }}" name="role" value="tondeur"> <label for="role_tondeur" class="form-label">{{ __('Tondeur') }}</label>
 
                 </div>
             </div>
             {{-- Fin role de l'utilisateur --}}
 
-            <div class="form-group row mb-0">
-                <div class="col-md-6 offset-md-4">
+            <div class="form-group">
+                <div class="btn btn-primary">
                     <button type="submit" class="btn btn-primary">
                         {{ __('C\'est parti !') }}
                     </button>
