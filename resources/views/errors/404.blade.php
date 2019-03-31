d5fe32a1d89d1fc545ed15ae1e701e77b6bd4b3b
DondaineMathieu <dondaine.mathieu@gmail.com>
2019-03-31 19:02:35 +0200

    erreur 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>TontonGazon - Erreur 404</title>
+</head>
+<body>
+    
+    <div id="container-error" style="position:absolute; top:0; left:0; right:0; width:100%; height:100%; background-image:url(/img/error.png); background-repeat:no-repeat; background-size:cover; background-position:center;">
+        <p style="font-weight:bold; position:absolute; top:40%; left:25%; transform:translateY(-50%); font-size:4rem; font-family: 'Aileron', sans-serif; color:white;">
+            Erreur 404
+            <a href="/" style="display:block; color:white;font-weight:normal;font-size:2rem;">Retourner vers le site</a>
+        </p>
+    </div>
+
+</body>
+</html>
