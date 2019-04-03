ca9ac850fd484fb778af17b571c7649fc9aaa238
DondaineMathieu <dondaine.mathieu@gmail.com>
2019-04-04 00:27:37 +0200

    changement message error

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -7,7 +7,7 @@
     
     <div id="container-error" style="position:absolute; top:0; left:0; right:0; width:100%; height:100%; background-image:url(/img/error.png); background-repeat:no-repeat; background-size:cover; background-position:center;">
         <p style="font-weight:bold; position:absolute; top:40%; left:25%; transform:translateY(-50%); font-size:4rem; font-family: 'Aileron', sans-serif; color:white;">
-            Erreur 404
+            Il n'y a rien a tondre ici,
             <a href="/" style="display:block; color:white;font-weight:normal;font-size:2rem;">Retourner vers le site</a>
         </p>
     </div>
