13733307676b5b188a01426bdddf5a313a2308aa
DondaineMathieu <dondaine.mathieu@gmail.com>
2019-04-05 00:15:57 +0200

    new favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,15 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+     <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon-16x16.png')}}">
+    <link rel="manifest" href="{{ asset('img/site.webmanifest')}}">
+    <link rel="mask-icon" href="{{ asset('img/safari-pinned-tab.svg')}}" color="#08b72c">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
     <!-- Scripts -->
     <script src="{{ asset('js/jquery.js') }}"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
