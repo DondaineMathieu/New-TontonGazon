a1005b3802c44710f2f3698ba3b094b8b9eb6f48
DondaineMathieu <dondaine.mathieu@gmail.com>
2019-04-04 18:45:27 +0200

    responsive header

--- resources/views/Elements/_header.blade.php
+++ resources/views/Elements/_header.blade.php
@@ -1,21 +1,28 @@
-<div id="menu-toogle">
-    <button class="hamburger hamburger--collapse" type="button">
+<div id="menuToggle">
+    <button id="button-hamburger" class="hamburger hamburger--collapse not-active" type="button">
         <span class="hamburger-box">
             <span class="hamburger-inner"></span>
         </span>
     </button>  
 </div>
 <nav class="nav">
+    <div>
+        <div id="logo-menu">
+            <a href="/">
+            <img src="/img/logo.png" alt="Logo"/>
+            </a>
+        </div>   
+    </div>
     <div id="menu">
         <ul>
             <li id="nav-V-ann"><a href="/annonces">Voir les annonces</a></li>
             <li id="nav-P-ann"><a href="/creer-annonce">Publier une annonce</a></li>
         @guest
             <li id="nav-connect"><a href="{{ route('login') }}">Connexion</a></li>
-            <li id="nav-inscription"><a href="{{ route('register') }}">Inscription</a></li>
+            <li class="li-last" id="nav-inscription"><a href="{{ route('register') }}">Inscription</a></li>
         @else
             <li id="nav-profil">Bonjour <b><a href="/utilisateur/{{ Auth::user()->id }}"> {{ Auth::user()->prenom }} </a></b></li>
-            <li id="nav-deconnect"><a href="{{ route('logout') }}" {{-- onclick="event.preventDefault(); document.getElementById('logout-form').submit();"--}}> Deconnexion </a></li>
+            <li class="li-last" id="nav-deconnect"><a href="{{ route('logout') }}" {{-- onclick="event.preventDefault(); document.getElementById('logout-form').submit();"--}}> Deconnexion </a></li>
         @endguest
     <ul>
     </div>
